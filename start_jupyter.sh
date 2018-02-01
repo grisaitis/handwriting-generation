1abab9cb4431d42d55a9df97691b6d4336924a7c
William Grisaitis <grisaitisw@janelia.hhmi.org>
2018-01-31 21:23:59 -0600

    Specify command, add tensorboard port, for jupyter docker run

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -6,5 +6,8 @@ source ./docker_settings.sh
 docker run \
     -it \
     -p 8888:8888 \
+    -p 6006:6006 \
     -v ${PWD}:${THIS_FOLDER_IN_CONTAINER} \
-    ${DOCKER_IMAGE}
+    -w /notebooks \
+    ${DOCKER_IMAGE} \
+    /run_jupyter.sh --allow-root
